11a376ab68a620ca01960ccec4f9b79266e1d01b
EverforgeWorks <everforgeworks@gmail.com>
2026-01-30 00:57:35 -0600

    Update star.go

--- internal/core/entity/star.go
+++ internal/core/entity/star.go
@@ -1,12 +1,13 @@
 package entity
 
-import (
-	"github.com/google/uuid"
-)
+import "github.com/google/uuid"
 
 type Star struct {
-	ID   uuid.UUID `json:"id" yaml:"-"`    // Ignored in YAML, generated in Go
-	Name string    `json:"name" yaml:"-"`  // Ignored in YAML, generated in Go
+	ID   uuid.UUID `json:"id" yaml:"id"`
+	Name string    `json:"name" yaml:"name"`
 	X    int       `json:"x" yaml:"x"`
 	Y    int       `json:"y" yaml:"y"`
+	// Future expansion:
+	// Resources  []Resource `json:"resources" yaml:"resources"`
+	// Planets    []Planet   `json:"planets" yaml:"planets"`
 }
